10abd29bf092846e52b137850cc95fb8fb15071d
ykurata <yasukokurata@gmail.com>
2020-05-13 22:50:36 -0400

    updated using react hooks

--- src/context.js
+++ src/context.js
@@ -1,43 +1,25 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios'
 
-const Context = React.createContext();
+export const Context = React.createContext();
 
-const reducer = (state, action) => {
-  switch(action.type){
-    case 'SEARCH_TRACKS':
-      return {
-        ...state,
-        track_list: action.payload,
-        heading: 'Search results'
-      };
-      default: 
-        return state;
-  }
-}
-
-export class Provider extends Component {
-  state = {
+export function ContextController({ children }) {
+  let initialState  = {
     track_list : [],
-    heading: 'Top 10 Tracks', 
-    dispatch: action => this.setState(state => reducer(state, action))
+    heading: ''
   }
 
-  componentDidMount() {
+  const [state, setState ] = useState(initialState);
+
+  useEffect(() => {
     axios.get(`https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=10&country=us&f_has_lyrics=1&apikey=${process.env.REACT_APP_MM_KEY}`)
       .then(res => {
         this.setState({track_list: res.data.message.body.track_list})
       })
       .catch(err => console.log(err));
-  }
+  }, []);
 
-  render() {
-    return (
-      <Context.Provider value={this.state}>
-        {this.props.children}
-      </Context.Provider>
-    )
-  }
+  return (
+    <Context.Provider value={[state, setState]}>{children}</Context.Provider>
+  );
 }
-
-export const Consumer = Context.Consumer;
